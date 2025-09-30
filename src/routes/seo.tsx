6b9827e800e1a3ee65b288c12639ac3888caab91
matoliya-shubham <matoliyaashubham@gmail.com>
2025-09-30 16:55:29 +0530

    minor changes

--- src/routes/seo.tsx
+++ src/routes/seo.tsx
@@ -20,17 +20,17 @@ function RouteComponent() {
   }, [navigate]);
 
   return (
-    <div className="h-full max-h-[var(--main-height)] overflow-y-auto py-4">
+    <div className="h-full max-h-[var(--main-height)] overflow-y-auto pb-4">
       <div className="flex flex-col">
-        <h1 className="text-2xl font-bold  ml-4">
+        <h1 className="text-2xl font-bold mt-4 ml-4">
           SEO keyword research Wizard
         </h1>
         <p className="text-gray-500 ml-4 ">
           Setup and track your keyword research workflow
         </p>
       </div>
-      <div className="flex flex-col justify-baseline items-center h-max gap-2 relative mt-6">
-        <div className="sticky top-0 grid grid-cols-2 gap-2 p-2 w-full md:w-2/3 mt-4 bg-white shadow-sm rounded-sm">
+      <div className="flex flex-col  justify-baseline items-center h-max gap-2 relative mt-6">
+        <div className="sticky top-0 grid grid-cols-2 gap-2 p-2 w-full md:w-2/3 mt-4 bg-white shadow-sm rounded-sm ">
           <Button variant="outline" asChild>
             <Link to="/seo/research">Research</Link>
           </Button>
